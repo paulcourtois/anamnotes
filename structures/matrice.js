0df6842c4f5fc7ddb3b6b0b7e9cc7778d72dc65c
paulcourtois <paulcourtoispaul@gmail.com>
2021-07-08 13:17:53 +0200

    q1,2 ok

---
+++ structures/matrice.js
@@ -0,0 +1,67 @@
+// Matrice est un tableau à 2 dimensions, ayant n lignes et p colonnes
+  // On notera Ai,j la valeur située à la ligne i et colonne j
+
+// Un vecteur est un tableau à une dimension
+
+// Soit M une matrice (avec n lignes et p colonnes) et V un vecteur (avec p composantes), le produit matriciel MV est un vecteur R ayant n composantes
+
+
+// Complexité du produit matriciel : nombre d'opérations nécessaires à sa résolution
+
+// lignes = n;
+// colonnes = p;
+
+
+// On crée une fonction qui représente une matrice
+let matrice = (n,p)=>{
+  let result = [];
+  for (i = 1; i < n+1; i++){
+    let row = [];
+    for (j = 1; j < p+1; j++){
+      row.push(`a${i}${j}`)
+    }
+    result.push(row);
+  }
+  return result
+}
+// Test
+console.log('ma matrice', matrice(3,2));
+
+
+// On crée une fonction qui crée un vecteur
+let vecteur = (p)=>{
+  let result = [];
+  for (k = 1; k < p+1; k++){
+    result.push(`x${k}`)
+  }
+  return result;
+}
+// Test
+console.log ('mon vecteur', vecteur(2));
+
+// On crée une fonction qui calcule un produit matriciel, à partir des fonctions précédentes
+// On ajoute un compteur pour calculer la complexité de l'opération
+let complexCounter = 0;
+let produitMatriciel = (n,p) =>{
+  let maMatrice = matrice(n,p);
+  let monVecteur = vecteur(p);
+
+  let produit = [];
+  for (i = 0 ; i < n ; i++){
+    let row = [];
+    for (j = 0; j < p; j++){
+      let column = maMatrice[i][j] + ' * ' + monVecteur[j];
+      complexCounter++
+      if (j <p-1){
+        column = column + ' + ' ;
+        complexCounter++ ;
+      }
+      row.push(column)
+    }
+    produit.push(row);
+  }
+  return produit
+}
+
+// TODO: meilleur rendu du produit
+console.log( produitMatriciel(3,2), 'complexité: ', complexCounter)
